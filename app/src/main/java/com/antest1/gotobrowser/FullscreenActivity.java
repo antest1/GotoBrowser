eae0430094a1de62346249f300796ce24528f245
antest1 <alias_n@live.jp>
2019-02-21 22:32:02 +0900

    remove soft key option

--- app/src/main/java/com/antest1/gotobrowser/FullscreenActivity.java
+++ app/src/main/java/com/antest1/gotobrowser/FullscreenActivity.java
@@ -91,7 +91,7 @@ public class FullscreenActivity extends AppCompatActivity {
             // at compile-time and do nothing on earlier devices.
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    // | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
