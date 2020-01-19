d98c2ed1a854bc839e631f35dbbefd222e4457d7
antest1 <alias_n@live.jp>
2020-01-20 00:09:08 +0900

    update kcs_cda.js

--- app/src/main/assets/kcs_cda.js
+++ app/src/main/assets/kcs_cda.js
@@ -1,49 +1,58 @@
 ﻿window.addEventListener('message', function (event) {
-	if (typeof event.data != "string") return;
+	if (typeof event.data == "string") {
+        // サーバ選択前
+        if (event.data == "w") {
+            if (ConstServerInfo.Gadget == event.origin + "/") {
+                Viewer_SendMessage();
+                return;
+            } else {
+                return;
+            }
+        }
 
-    // サーバ選択前
-    if (event.data == "w") {
-        if (ConstServerInfo.Gadget == event.origin + "/") {
-            Viewer_SendMessage();
-            return;
-        } else {
+        // サーバ選択後
+        if (event.data == "r") {
+            if (ConstServerInfo.Gadget == event.origin + "/") {
+                kcsLogin_StartLogin();
+                return;
+            } else {
+                return;
+            }
+        }
+
+        // ゲームサーバー以外の呼び出しや空のデータに対しては何もしない
+        if ((ConstServerInfo.NETGAME != event.origin && userWorldInfo.worldServerAddr != event.origin) || event.data == "") {
             return;
         }
-    }
-
-    // サーバ選択後
-    if (event.data == "r") {
-        if (ConstServerInfo.Gadget == event.origin + "/") {
-            kcsLogin_StartLogin();
+        // データの種別により処理を分岐
+        var data = event.data.split("\t");
+        if (data[0] == "0") {
+            // 0: アイテム購入
+            kcsPayment_StartPayment(data[1], data[2], data[3], data[4], data[5], data[6]);
             return;
-        } else {
+        } else if(data[0] == "1") {
+            // 1: コメント変更
+            kcsInspection_CreateInspectionComment(data[1]);
+            return;
+        } else if(data[0] == "2") {
+            // 2: 艦隊名変更
+            kcsInspection_CreateInspectionDeckName(data[1]);
+            return;
+        } else if(data[0] == "3") {
+            // 3: 提督名変更
+            kcsInspection_CreateInspectionNickName(data[1]);
+            return;
+        } else if(data[0] == "5") {
+            // 5: 設定情報を cookie に保存する
+            var options = data[1];
+            kcsOptions_Save(options);
+            return;
+        } else if(data[0] == "6") {
+            // 6: 設定情報を cookie から取得してゲーム側へ送信する
+            Options_SendMessage(kcsOptions_Load());
             return;
         }
-    }
-
-    // ゲームサーバー以外の呼び出しや空のデータに対しては何もしない
-    if ((ConstServerInfo.NETGAME != event.origin && userWorldInfo.worldServerAddr != event.origin) || event.data == "") {
-        return;
-    }
-    // データの種別により処理を分岐
-    var data = event.data.split("\t");
-    if (data[0] == "0") {
-        // 0: アイテム購入
-        kcsPayment_StartPayment(data[1], data[2], data[3], data[4], data[5], data[6]);
-        return;
-    } else if(data[0] == "1") {
-        // 1: コメント変更
-        kcsInspection_CreateInspectionComment(data[1]);
-        return;
-    } else if(data[0] == "2") {
-        // 2: 艦隊名変更
-        kcsInspection_CreateInspectionDeckName(data[1]);
-        return;
-    } else if(data[0] == "3") {
-        // 3: 提督名変更
-        kcsInspection_CreateInspectionNickName(data[1]);
-        return;
-    }
+	}
 }, false);
 
 function PaymentResult_SendMessage(paymentResult) {
@@ -70,5 +79,14 @@ function Viewer_SendMessage() {
 
     if (gameElement) {
         gameElement.postMessage(viewerInfo.id, ConstServerInfo.Gadget);
+    }
+}
+
+function Options_SendMessage(options) {
+
+    var gameElement = document.getElementById("htmlWrap").contentWindow;
+
+    if (gameElement) {
+        gameElement.postMessage(options, userWorldInfo.worldServerAddr);
     }
 }
