d28bc4f171e758ede21cf9e640055a2205315f85
antest1 <kcanotify@gmail.com>
2023-03-21 20:30:37 +0900

    fix notification crash (android 12)

--- app/src/main/java/com/antest1/gotobrowser/Notification/ScreenshotNotification.java
+++ app/src/main/java/com/antest1/gotobrowser/Notification/ScreenshotNotification.java
@@ -7,7 +7,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
@@ -16,8 +15,6 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.antest1.gotobrowser.R;
-
-import static com.antest1.gotobrowser.Constants.PREF_NC_SCREENSHOT_SET;
 
 public class ScreenshotNotification {
     public static final String NOTI_CHANNEL_SCREENSHOT = "NOTI_CHANNEL_SCREENSHOT";
@@ -32,17 +29,11 @@ public class ScreenshotNotification {
     }
 
     private void registerScreenshotNotificationChannel(Context context) {
-        String preference_key = context.getString(R.string.preference_key);
-        SharedPreferences sharedPref = context.getSharedPreferences(preference_key, Context.MODE_PRIVATE);
-        boolean isNcScreenshotSet = sharedPref.getBoolean(PREF_NC_SCREENSHOT_SET, false);
-        if (!isNcScreenshotSet && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String name = context.getString(R.string.noti_screenshot_name);
-            int importance = NotificationManager.IMPORTANCE_LOW;
-            NotificationChannel mChannel = new NotificationChannel(NOTI_CHANNEL_SCREENSHOT, name, importance);
+            NotificationChannel mChannel = new NotificationChannel(NOTI_CHANNEL_SCREENSHOT, name, NotificationManager.IMPORTANCE_LOW);
+            NotificationManager manager = (NotificationManager) context.getSystemService(NotificationManager.class);
             manager.createNotificationChannel(mChannel);
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putBoolean(PREF_NC_SCREENSHOT_SET, true).apply();
         }
     }
 
@@ -51,7 +42,7 @@ public class ScreenshotNotification {
         intent.setAction(android.content.Intent.ACTION_VIEW);
         intent.setDataAndType(uri, "image/png");
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent contentIntent = PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent contentIntent = PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         Notification notification = new NotificationCompat.Builder(activity, NOTI_CHANNEL_SCREENSHOT)
                 .setSmallIcon(R.drawable.ic_noti_screenshot)
